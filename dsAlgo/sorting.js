871b6335d59e67da31eb74bf28cf40f9d147dd9a
Vishwa Mohan <vimohan@vimohan-mn2.linkedin.biz>
2022-02-14 23:10:07 +0530

    sorting questions

--- dsAlgo/sorting.js
+++ dsAlgo/sorting.js
@@ -1,33 +1,68 @@
 /**
  * Code for the bubble sort
+ * 
+ * no of comaprisons : n*n  Worst | Best
+ * 
+ * No swaps : worst : n*n   | Best case  : 0
+ * 
+ * TC BS : Best case : O(n)   | Worst Case : O(n*n)
+ * 
+ * 
+ * Stable or not : Stable
  */
 
 function bubbleSort(arr){
  
-   for( i = arr.length -1 ;i>=0;i--){
+   for( i = arr.length -1 ;i>=0;i--){   //n  1
 
-        //In every iteration of this for loop, I want to bring
-        //one element to it's right position
-       for(j=0;j<i;j++){
+       /**
+        * Inner  for loop has the logic :
+        * 1.to compare and swap
+        * 
+        * Array is already sorted
+        * 
+        * will there be any swapping ?  - No
+        * 
+        * When swapping happens ? -- When it goes inside the if block
+        *  */ 
+       var isSorted = true ;
+       for(j=0;j<i;j++){  //  n
             //if left is bigger than right, swap
-            if(arr[j]>arr[j+1]){
+            if(arr[j]>arr[j+1]){   // n*n
+                //No sorted , if it comes here
+                isSorted = false;
                 var temp = arr[j];
                 arr[j]=arr[j+1];
                 arr[j+1]=temp;
             }
        }}
 
+       if(isSorted){
+           return;
+       }
+
 }
 
+/**
+ * 
+ *No of comparison : n*n  Worst | Best 
 
+ No of Swaps :  n times | Worst | Best
+
+ Stable or unstable  : Unstable
+
+ InPlace ? - InPlace
+
+
+ */
 function selectionSort(arr){
 
    for(i=0;i<arr.length;i++){  //n
        var min_index =i ;
+       
        //find the index of the minumum element from i->n
-
        for(j=i+1;j<arr.length;j++){  //n
-           if(arr[j]<arr[min_index]){
+           if(arr[j]<arr[min_index]){  // comparison
                min_index =j;
            }
        }
@@ -49,18 +84,30 @@ function selectionSort(arr){
 
 /**
  * Insertion Sort cod
+ * 
+ * TC : Worst case : n*n  Best Case : O(n)
+ * 
+ * Stability : Stable
+ * 
+ * No of comparisons :   Worst case -- n*n  , best case -- n
+ * 
+ * No of shifts : Worst case - n*n , best case - n
  */
 
 function insertionSort(arr){
+   /**
+    * Start for second element : i=1
+    */
+   for(i=1;i<arr.length;i++){  // n
+        var v = arr[i];  // store the elememts to be compared
 
-   for(i=1;i<arr.length;i++){
-        var v = arr[i];
+        //We need to keep comparing to the elements in the left, till I find a number greater than v
         j=i-1;
-        while(j>=0 && arr[j]>v){
-            arr[j+1] = arr[j];
+        while(j>=0 && arr[j]>v){ //Will not execute
+            arr[j+1] = arr[j];  // Shift the element to the right
             j--;
         }
-        arr[j+1]=v;
+        arr[j+1]=v; // set the v at the right place
     }
 }
 
@@ -90,11 +137,11 @@ function fact( num ){
 
 arr = [1,3,5,4,2,9];
 
-console.log(arr);
+console.log("My arr", arr);
 
 mergeSort(arr, 0, 5);
 
-console.log(arr);
+console.log("My arr after sorting", arr);
 
 
 
@@ -143,9 +190,6 @@ function mergeSort(arr, start, end) {
  */
 function merge(arr, start, mid , end){   // TC -> O(n) | SC -> O(n)
 
-    //sorted start->mid and mid+1->end
-
-    //Create a temp array of the same size as that of arr
     var tempArr = new Array(arr.length);  // Extra space
     var tempIndex = start; // this will be used to track the index in the temorary array
 
@@ -153,10 +197,7 @@ function merge(arr, start, mid , end){   // TC -> O(n) | SC -> O(n)
 
     var rStart = mid+1; //starting index of right half
 
-    /**
-     * Write the logic to insert elements in the tempArray in sorted order
-     */
-    while(lStart<=mid && rStart<=end){
+   while(lStart<=mid && rStart<=end){
         if(arr[lStart]<=arr[rStart]){ //element in left part is smaller than right part
              //put the element frm left into the tempArray
              tempArr[tempIndex] = arr[lStart];
@@ -169,27 +210,24 @@ function merge(arr, start, mid , end){   // TC -> O(n) | SC -> O(n)
             rStart++;
         }
     }
-    //If the elements are still left in the left array : lStart<=mid
-
     while(lStart<=mid){
         tempArr[tempIndex++]= arr[lStart++];
     }
 
-    //If the elements are still left in the right array : rStat<=end
-
     while(rStart<=end){
         tempArr[tempIndex++] = arr[rStart++];
     }
-
-    //tempArr will have all the numbers in the sorted order
-
-    //Need to copy the numbers from tempArr to the original
     for(i=start;i<=end;i++){
         arr[i]=tempArr[i];
     }
 
 }
 
+
+
+var myArr = [ 4,3,1,5,2];
+
+console.log(myArr);
 
 /**
  * Find the inversions in an array
@@ -336,11 +374,112 @@ function swap(arr, a, b){
 }
 
 
+qArray = [5,6,2,8,9,40];
+
+console.log("qArr", qArray);
+
+quickSort(qArray,0,5);
+
+console.log("qArr", qArray);
 
 
+/**
+ * Merge two arras
+ * m-> no of elements in arr1
+ * n -> n of element in arr2
+ * 
+ * m+n = arr1.length
+ */
 
-start = 0 ;
-end = 7 ;
-console.log(Math.floor(Math.random()*(end-start+1) +start))
+function mergeWithoutSpace(arr1,m, arr2, n){
+
+    t1 = m-1;
+    t2 = n-1;
+
+    k = arr1.length-1;
+
+    while(t1>=0 && t2>=0){
+        if(arr1[t1]>arr2[t2]){
+            arr1[k--]=arr1[t1--];
+        }else{
+            arr1[k--]=arr2[t2--];
+        }
+    }
+
+    while(t1>=0){  // Redundant code -- written to show similarity with merge sort
+        arr1[k--]=arr1[t1--];
+    }
+
+    while(t2>=0){
+        arr1[k--]=arr2[t2--];
+    }
+
+}
 
 
+/**
+ * 
+ *  countInversion(arr, 1,4) 
+ */
+function countInversions(arr, start, end){
+
+    //Base condition
+    if(start>=end){
+        return 0 ;
+    }
+
+    var mid = start + parseInt((end-start)/2);
+
+    var leftCount = countInversions(arr, start, mid);
+    var rightCount = countInversions(arr, mid+1, end);
+
+    //Count of the inversion which involve left and right both
+    var countLeftRight = countMerge(arr, start, mid, end);
+
+    return leftCount + rightCount + countLeftRight ;
+
+}
+/**
+ * 
+ * This will try to find the number of inversions in left to right
+ */
+function countMerge(arr, start, mid, end){
+
+    var tempArr = new Array(arr.length);
+
+    var tIndex = start;
+    var lStart = start;
+    var rStart = mid+1;
+
+    var count=0;
+
+    while(lStart<=mid && rStart<=end){
+        if(arr[lStart]<=arr[rStart]){
+            tempArr[tIndex++]=arr[lStart++];
+        }else{
+            //this is the case of inversion i.e lstart < rStart but arr[lstart]>arr[rStart]
+            count = count + mid-lStart+1;
+            tempArr[tIndex++] = arr[rStart++]; 
+        }
+    }
+
+    while(lStart<=mid){
+        tempArr[tIndex++]=arr[lStart++];
+    }
+
+    while(rStart<=end){
+        tempArr[tIndex++] = arr[rStart++]; 
+    }
+
+    for(i=start;i<=end;i++){
+        arr[i]=tempArr[i];
+    }
+
+    return count ;
+
+}
+
+
+var myArr = [9, 4, 3, 5, 1];
+
+console.log(countInversions(myArr,0,4));
