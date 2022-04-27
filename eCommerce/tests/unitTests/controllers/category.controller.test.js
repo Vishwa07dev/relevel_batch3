c62309410b3971c50d93956862fe88e5128c2351
Vishwa Mohan <vimohan@vimohan-mn2.linkedin.biz>
2022-04-27 10:29:59 +0530

    class code

--- eCommerce/tests/unitTests/controllers/category.controller.test.js
+++ eCommerce/tests/unitTests/controllers/category.controller.test.js
@@ -17,6 +17,7 @@ const categoryController = require("../../../controllers/category.controller");
  * Mocked req and resp is provided by inteceptor.js file
  */
 let req, res;
+
 beforeEach(() => {
     req = mockRequest();
     res = mockResponse();
@@ -24,14 +25,18 @@ beforeEach(() => {
 
 describe("Teating create category method", () => {
 
-    it('test successfull creation of a new category',async () => {
-
+    beforeEach(()=>{
         //For creating the category, req should have a body
         req.body = newCategory
+    })
+
+    it('test successfull creation of a new category', async () => {
+
+        
 
         //Mock and spy on Category create method
         const spy = jest.spyOn(Category, 'create').mockImplementation((newCategory) => Promise.resolve(newCategory));
-        
+
         //Execute the create method
 
         await categoryController.create(req, res);
@@ -46,15 +51,102 @@ describe("Teating create category method", () => {
         expect(res.status).toHaveBeenCalledWith(201);
         // res send is sending the newCategory
         expect(res.send).toHaveBeenCalledWith(newCategory);
- 
+
     });
 
     /**
      * Testing the failure scenario
      */
-    it("test failure during the  creation of a new category", ()=>{
+    it("test failure during the  creation of a new category", async () => {
+
         
+        //Mock and spy
+        const spy = jest.spyOn(Category, 'create').mockImplementation(() => Promise.reject(Error("Error while creating")));
+
+        //Execute the method
+        await categoryController.create(req, res);
+
+        await expect(spy).toHaveBeenCalled();
+        expect(Category.create).toHaveBeenCalledWith(newCategory);
+        expect(res.status).toHaveBeenCalledWith(500);
+        expect(res.send).toHaveBeenCalledWith({
+            message :  "Some internal error happened"
+        });
+    });
+
+
+});
+
+
+describe("Testing the findAll method", ()=>{
+
+
+
+    it("Test the find all mehtod when no query param was passed", async ()=>{
+
+        //Mock the category.findAll method
+        const spy = jest.spyOn(Category, 'findAll').mockImplementation(()=>Promise.resolve(newCategory));
+        
+        //Invoke the method
+        await categoryController.findAll(req, res);
+
+        //Validations
+        expect(spy).toHaveBeenCalled();
+        expect(Category.findAll).toHaveBeenCalled();
+        expect(res.status).toHaveBeenCalledWith(200);
+        expect(res.send).toHaveBeenCalledWith(newCategory);
+
+
+    });
+
+    it("Test the find all method with the query param", async()=>{
+        const queryParam = {
+            where : {
+                name : "Electronics"
+            }
+        };
+
+        const spy = jest.spyOn(Category, 'findAll').mockImplementation((queryParam)=> Promise.resolve(newCategory));
+
+        req.query = {
+            name : 'Electronics'
+        }
+
+        //Execute the method
+        await categoryController.findAll(req, res);
+
+        //Validations
+        await expect(spy).toHaveBeenCalled();
+        expect(Category.findAll).toHaveBeenCalledWith(queryParam);
+        expect(res.status).toHaveBeenCalledWith(200);
+        expect(res.send).toHaveBeenCalledWith(newCategory);
+
     })
+})
+
+describe("Testing update method", ()=>{
+    it("testing successfull update",async ()=>{
+
+        req.body = newCategory;
+        req.params = {
+            id:1
+        }
+        //Category update method has to be mocked
+
+        const spyOnUpdate = jest.spyOn(Category,'update').mockImplementation(()=> Promise.resolve(newCategory));
+
+        const spyOnFindByPK = jest.spyOn(Category, 'findByPk').mockImplementation(()=>Promise.resolve(newCategory));
 
 
+        //Execute
+        await categoryController.update(req, res);
+
+        //Validation
+
+        await expect(spyOnUpdate).toHaveBeenCalled();
+        await expect(spyOnFindByPK).toHaveBeenCalled();
+        expect(Category.update).toHaveBeenCalled();
+
+
+    })
 })
