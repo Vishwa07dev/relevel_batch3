8c03e7ff3bade979efb57fef720451ad3bf7c02c
Vishwa Mohan <vimohan@vimohan-mn2.linkedin.biz>
2022-05-01 17:25:38 +0530

    git commands

---
+++ eCommerce/github.js
@@ -0,0 +1,24 @@
+/**
+ * Steps for the question asked in the class
+ * Raise a PR to the github repo : https://github.com/Vishwa07dev/DemoGitHubRepo/
+ * 
+ * 
+ * Step 1 : Fork it to your remote repo
+ * Step 2 : Clone it locally 
+ *          git clone <url>
+ * Step 3 : Create a branch - student
+ *          git checkout -b student
+ * Step 4 : Create a file inside the folder abc.txt. I am Vishwa
+ * Step 5 : git add .
+ * Step 6 : git commit -m "Commiting the new file"
+ * Step 7 : git push -u origin student
+ * 
+ * Step 8 : Come to the github url and raise the PR again  Vishwa07dev/DemoGitHubRepo 
+ * main branch
+ * 
+ * 
+ * 
+ * 
+ */
+
+
